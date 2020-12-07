24747f9296e0af4cceeb43709bda0b3caa7fe860
Mauricio <mauricio.caicedo.palacio@correounivalle.edu.co>
2020-12-07 09:21:46 -0500

    Primera version FastApi

---
+++ models/transaction_models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class TransactionIn(BaseModel):
+    username: str 
+    value:int
+
+class TransactionOut(BaseModel):
+    id_transaction: int
+    username: str
+    date: datetime
+    value: int
+    actual_balance: int
