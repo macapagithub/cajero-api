89f849cc2e7c0c8ea2a17314e24608b746ebe34b
Mauricio <mauricio.caicedo.palacio@correounivalle.edu.co>
2020-12-10 23:09:29 -0500

    Connect back-front

--- main.py
+++ main.py
@@ -7,11 +7,24 @@ from db.transaction_db import save_transaction
 from models.user_models import UserIn, UserOut
 from models.transaction_models import TransactionIn, TransactionOut
 
+
 import datetime 
 from fastapi import FastAPI 
 from fastapi import HTTPException
 api = FastAPI()
 
+
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost.tiangolo.com","https://localhost.tiangolo.com",
+    "http://localhost","http://localhost:8080",
+]
+
+api.add_middleware(
+    CORSMiddleware, allow_origins = origins,
+    allow_credentials = True, allow_methods = ["*"], allow_headers = ["*"],
+)
 
 @api.get("/")
 async def home():
